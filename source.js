73c4504da47c81958eaaf1d061879b796c4bdd27
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-04-09 19:24:33 +0000

    created initial project

---
+++ source.js
@@ -0,0 +1,13 @@
+var dateField = document.getElementById("date")
+var timeField = document.getElementById("time")
+
+function setTime(){
+    let currentDate = new Date();
+    timeField.innerText = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+
+
+
+    dateField.innerText = `${currentDate.toLocaleDateString()}`
+}
+
+setInterval(setTime, 1000)
