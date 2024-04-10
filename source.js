2592f95f0f58327a5e609cca34ba5b6ee5cd7b46
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-04-10 17:37:48 +0000

    changed bg color to gray

--- source.js
+++ source.js
@@ -8,7 +8,13 @@ function setTime(){
     let seconds = currentDate.getSeconds()
 
     if(hours.length<2){
-        hours = hours.
+        hours = "0"+hours;
+    }
+    if(minutes.length<2){
+        minutes = "0"+minutes;
+    }
+    if(seconds.length<2){
+        seconds = "0"+seconds;
     }
     timeField.innerText = `${hours}:${minutes}:${seconds}`
     dateField.innerText = `${currentDate.toLocaleDateString()}`
